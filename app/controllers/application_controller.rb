59935d2fbd5c9ab683d959b25fe669280432e902
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-07-07 13:44:30 -0500

    mensajes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@
 class ApplicationController < Sip::ApplicationController
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
-    render :file => "#{Rails.root}/public/403.html", 
-      :status => 403, :layout => true
+    redirect_to Rails.application.routes.url_helpers.root_path, 
+      :alert => exception.message
   end
 end
 
