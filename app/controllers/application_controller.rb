642ff5a97b11fa4e006c4102fbcdd02d9ff26546
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-07-07 09:38:19 -0500

    Redirige si expira sesion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # encoding: UTF-8
-require 'sip/application_controller'
+
 class ApplicationController < Sip::ApplicationController
   protect_from_forgery with: :exception
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", 
+      :status => 403, :layout => true
+  end
 end
 
