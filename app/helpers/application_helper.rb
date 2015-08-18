b779f7aa4116c3210f02c89b40583fe2858e9a54
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-08-17 20:38:46 -0500

    simplifica

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,7 @@
 # encoding: UTF-8
 
 module ApplicationHelper
+
   include Sal7711Gen::ApplicationHelper
 
-	def pagina(collection, params= {})
-		# Solución de https://gist.github.com/jeroenr/3142686
-		will_paginate collection, params.merge(
-			:renderer => PaginacionAjaxHelper::GeneraEnlace
-		)
-	end
 end
