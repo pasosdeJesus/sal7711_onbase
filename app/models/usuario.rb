1c803a875160240a41673b9998d0e8824e3020ed
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-10-27 11:30:09 -0500

    auxiliar para determinar cuando se autentica por ip

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,4 +4,8 @@ class Usuario < Sal7711Gen::Usuario
   devise :registerable, :confirmable
 
   validates_length_of :nusuario, maximum: 255
+
+  @autenticado_por_ip = false
+  attr_accessor :autenticado_por_ip
+
 end
