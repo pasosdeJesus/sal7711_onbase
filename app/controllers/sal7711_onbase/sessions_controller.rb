9834e105bf05a28bdd264d1bf6cd57b9adfc2159
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-11-13 11:12:43 -0500

    Al intentar autenticar con usuario y clave, sale de autenticado por ip

--- app/controllers/sal7711_onbase/sessions_controller.rb
+++ app/controllers/sal7711_onbase/sessions_controller.rb
@@ -3,6 +3,9 @@
 require 'devise/sessions_controller'
 
 class Sal7711Onbase::SessionsController < Devise::SessionsController
+  #http://stackoverflow.com/questions/8570077/about-overriding-devise-or-clearance-controllers
+  skip_before_filter :require_no_authentication
+
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
@@ -10,5 +13,21 @@ class Sal7711Onbase::SessionsController < Devise::SessionsController
     yield if block_given?
     respond_to_on_destroy
     #super
+  end
+
+  def new
+    if current_usuario
+      if current_usuario.autenticado_por_ip
+        sign_out(current_usuario)
+      else
+        return
+      end
+    end
+    super
+    if current_usuario
+      puts "OJO no autentico por IP";
+      current_usuario.autenticado_por_ip = false
+      current_usuario.save!
+    end
   end
 end
